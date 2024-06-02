e3edf8712612dba73b9330ffe91836fbb14158d6
StanislavN <s.neshik@yandex.ru>
2024-06-02 13:06:23 +0300

    Add arrowBTN

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,31 @@
 import arrow from 'src/images/arrow.svg';
-
+import { MouseEvent, useState, useEffect } from "react";
 import styles from './ArrowButton.module.scss';
+import clsx from 'clsx';
+
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+export const ArrowButton = ()=> {
+
+const [isOpen, setIsOpen] = useState<boolean>(false);
+		
+ let click = ():void => {
+if (isOpen === false) {setIsOpen(true)}
+else setIsOpen(false)
+
+ }
+
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
+		onClick={click}
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(styles.container, {[styles.container_open]: isOpen})}>
+			<img src={arrow} alt='иконка стрелочки' className={clsx(styles.arrow, {[styles.arrow_open]: isOpen})} />
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,5 +1,8 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Separator } from '../separator'
+import { Select } from '../select/index'
+import {RadioGroup} from '../radio-group/index'
 
 import styles from './ArticleParamsForm.module.scss';
 
@@ -7,8 +10,24 @@ export const ArticleParamsForm = () => {
 	return (
 		<>
 			<ArrowButton />
-			<aside className={styles.container}>
+			<aside className={`${styles.container} `}>
 				<form className={styles.form}>
+				<Select placeholder='ggg' selected={null} options={[]} title={'ШРИФТ'}/>
+				<Select placeholder='ggg' selected={null} options={[]} title={'ЦВЕТ ШРИФТА'}/>
+
+				<RadioGroup name='Жопа' options={  [
+
+		{ title: '1 опция', value: '1 опция', className: '' },
+		{ title: '2 опция', value: '2 опция', className: '' },
+		{ title: '3 опция', value: '3 опция', className: '' },
+
+	] } title='Попа' selected = {{title: '12345', value: '3', className: '3'}} />
+
+
+				<Separator />
+				<Select placeholder='ggg' selected={null} options={[]} title={'ЦВЕТ ФОНА'}/>
+				<Select placeholder='ggg' selected={null} options={[]} title={'ШИРИНА КОНТЕНТА'}/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
